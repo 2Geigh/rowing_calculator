190dd1a587d6362b927b01f3a4d786450f9a66e7
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-09-06 19:06:18 -0400

    main.js scaffold quasi-pseudocode

---
+++ app/assets/js/main.js
@@ -0,0 +1,12 @@
+console.log('Hi mom');
+
+// Get the inputs
+
+// remove event listeners from the input form button
+
+// Add event listener to input form button to wrangle outputs
+
+// Create the function that draws the output once the input is calculated
+
+// Function for computing the outputs
+
