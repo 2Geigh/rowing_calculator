1e0ac0362fef1e747aadc901cfe64831dfae147b
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-09-14 17:03:02 -0400

    added visx and d3 npm packages

--- ergcalc/src/components/output_graphs/OutputGraphForSplitMode.jsx
+++ ergcalc/src/components/output_graphs/OutputGraphForSplitMode.jsx
@@ -1,7 +1,8 @@
-import { useRef, useEffect } from "react";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import { Bar, LinePath } from "@visx/shape";
 
-const OutputGraphForSplitMode = ({ computedData, OutputGraphRender, setOutputGraphRender}) => {
-    console.log(computedData);
+const OutputGraphForSplitMode = ({ computedData, OutputGraphRender, setOutputGraphRender, width = 400, height = 200}) => {
+    // console.log(computedData);
 
     // Compute XY coordinates for each point (one per division)
     let number_of_divisions = computedData.number_of_divisions;
@@ -32,18 +33,70 @@ const OutputGraphForSplitMode = ({ computedData, OutputGraphRender, setOutputGra
         x_coordinates_relative.push(relative_x);
     };
 
+    console.log(`point_coordinates_absolute: ${point_coordinates_absolute}`);
+    console.log(`x_coordinates_absolute: ${x_coordinates_absolute}`);
 
-    const ref = useRef();
-    // By having ref={ref} on an element, ref.current sets ref.current = <DOM element>  AFTER THE COMPONENT MOUNTS
-    // Therefore allowing you to apply whatever methods and stuff you want onto the real HTML element
-    // const svg_element = ref.current;
+
+    const xScale = scaleLinear({
+        domain: [0, Math.max(...x_coordinates_absolute)], // the categorical labels
+        range: [0, width],                            // pixel span horizontally
+        padding: 0.4                                  // space between bars
+    }); // xScale("A") returns the left-pixel coordinate for Bar A
+
+    const yScale = scaleLinear({
+        domain: [0, Math.max(...y_coordinates_absolute.map((y) => y))], // Numeric min and max of data
+        range: [height, 0] // Range in pixels, inverted because in SVG 0 is at the top
+    });
+
+
+    const barWidth = width / number_of_divisions;
+    const barCoordinates = []
+    // const barCoordinates = point_coordinates_absolute.map((x) => {x});
+    for (let i = 0; i < number_of_divisions; i++) {
+        let barX = i * ((number_of_divisions - 1) * (width / number_of_divisions));
+        barCoordinates[i] = [barX, y_coordinates_absolute[i]];
+    };
+    console.log(`barCoordinates: ${barCoordinates}`);
+    
+
+
     
     return(
+    <>
+        <svg width={width} height={height} className="bg-red-100">
+            {/* BARS */}
+            {barCoordinates.map(p => {
+                const barHeight = height - yScale(p[1]);
+                const barX = xScale(p[0]);
+                const barY = yScale(p[1]);
+            
+            
+                return (
+                    <Bar
+                        key={p[0]}
+                        x={barX}
+                        y={barY}
+                        width={barWidth}
+                        height={barHeight}
+                        fill="steelblue"
+                        stroke="black"
+                        strokeWidth={1}   
+                    >    
+                    </Bar>
+                );
+            })}
 
-    <div id="outputGraphContainer" className="border-2 border-gray-200 border-solid">
-        <svg id="outputGraph" ref={ref}></svg>
-    </div>
-    
+            {/* LINE */}
+            <LinePath
+                data={point_coordinates_absolute}
+                x={p => xScale(p[0])}
+                y={p => yScale(p[1])}
+                stroke="black"
+                strokeWidth={2}
+            >
+            </LinePath>
+        </svg>
+    </>
     );
 };
 
